ef2d85f9963dac86dffd48570d911d9b4ceee71c
Darsh <darsh2987@gmail.com>
2021-02-05 18:25:46 +0000

    function to animate hamburger icon

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,17 @@
 import "../scss/imports.scss";
+
+// Load Event - run script once window has loaded
+window.addEventListener("load", () => {
+  function hamburger() {
+    const hamburgerBtn = document.querySelector("#hamburger");
+    hamburgerBtn.addEventListener("click", () => {
+      if (hamburgerBtn.classList.contains("open")) {
+        hamburgerBtn.classList.remove("open");
+      } else {
+        hamburgerBtn.classList.add("open");
+      }
+    });
+  }
+
+  hamburger();
+});
