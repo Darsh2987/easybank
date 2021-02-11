4b2f898f0bdea7e791f8bc3b3cb3c59267e02275
Darsh <darsh2987@gmail.com>
2021-02-11 00:54:53 +0000

    modified hamburger event listener

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,11 +4,14 @@ import "../scss/imports.scss";
 window.addEventListener("load", () => {
   function hamburger() {
     const hamburgerBtn = document.querySelector("#hamburger");
+    const headerLinks = document.querySelector(".header__links");
     hamburgerBtn.addEventListener("click", () => {
       if (hamburgerBtn.classList.contains("open")) {
         hamburgerBtn.classList.remove("open");
+        headerLinks.classList.remove("mobile-nav--animate");
       } else {
         hamburgerBtn.classList.add("open");
+        headerLinks.classList.add("mobile-nav--animate");
       }
     });
   }
